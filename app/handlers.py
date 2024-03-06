e6a744c8d5308fcb8654192c5ae91199777b2f8c
Rostya <rostyamudrik@proton.me>
2024-03-06 19:41:26 +0200

    Callbacks added

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,6 @@
 from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 import app.keyboards as kb
 
@@ -13,7 +13,7 @@ async def cmd_start(message: Message):
     await message.reply(f'''Hello!
 Your user id: {message.from_user.id}
 firstname: {message.from_user.first_name}
-lastname: {message.from_user.last_name}''', reply_markup=kb.main)
+lastname: {message.from_user.last_name}''', reply_markup=kb.main_cb)
 
 
 @router.message(Command('help'))  # Ловим команду /help
@@ -35,3 +35,36 @@ async def get_id_of_photo(message: Message):
 async def get_photo(message: Message):
     await message.answer_photo(photo='AgACAgIAAxkBAAIBKmXnitcsaFF_GmG0iYBLY8v2pyebAAK41zEblXw4S1uVAfgYrd2AAQADAgADeQADNAQ',
                                caption='It\'s Ivan Zolo2004 and n3koglai')  # Также вместо id можно указывать ссылку на фото
+
+
+# Callbacks
+
+@router.callback_query(F.data == 'catalog')  # Ловим указанный callback
+async def catalog_callback(callback: CallbackQuery):
+    """Callback controller to show catalog"""
+
+    # Чтобы не загружалось просто так оставить пустым, или можно вывести всплывающее сообщение указав текст
+    await callback.answer('You chose catalog.', show_alert=True)  # show_alert=True для появления окошка с кнопкой ОК
+
+    await callback.message.answer('It\'s callback response')
+
+
+@router.callback_query(F.data == 'cars')  # Ловим указанный callback
+async def cars_callback(callback: CallbackQuery):
+    """Callback controller to display cars buttons and show alert"""
+    await callback.answer('cars', show_alert=True)
+    await callback.message.edit_text('cars', reply_markup=await kb.inline_cars())  # Edit text
+
+
+@router.callback_query(F.data == 'contacts')  # Ловим указанный callback
+async def contacts_callback(callback: CallbackQuery):
+    """Callback controller to display contacts buttons"""
+    await callback.answer('')
+    await callback.message.edit_text('contacts', reply_markup=kb.contacts)  # Edit text
+
+
+@router.callback_query(F.data == 'back_to_start')  # Ловим указанный callback
+async def back_to_start_callback(callback: CallbackQuery):
+    """Callback controller to back to start"""
+    await callback.answer('')
+    await callback.message.edit_text('back to start', reply_markup=kb.main_cb)  # Edit text
--- app/keyboards.py
+++ app/keyboards.py
@@ -5,9 +5,14 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Catalog')],
-    [KeyboardButton(text='Cart'), KeyboardButton(text='Contacts')],
+    [KeyboardButton(text='Basket'), KeyboardButton(text='Contacts')],
 ], resize_keyboard=True, one_time_keyboard=True, input_field_placeholder='Select anything...')
 
+main_cb = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Catalog', callback_data='catalog')],
+    [InlineKeyboardButton(text='Cars', callback_data='cars'),
+     InlineKeyboardButton(text='Contacts', callback_data='contacts')]
+])
 
 settings = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='YouTube', url='https://www.youtube.com/@sudoteach')]
@@ -22,3 +27,18 @@ async def inline_cars():
     for car in cars:
         keyboard.add(InlineKeyboardButton(text=car, url='https://www.youtube.com/@sudoteach'))
     return keyboard.adjust(2).as_markup()  # Количество кнопок в ряду
+
+
+contacts = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text='contact 1', url='https://www.youtube.com/@sudoteach'),
+        InlineKeyboardButton(text='contact 2', url='https://www.youtube.com/@sudoteach'),
+    ],
+    [
+        InlineKeyboardButton(text='contact 3', url='https://www.youtube.com/@sudoteach'),
+        InlineKeyboardButton(text='contact 4', url='https://www.youtube.com/@sudoteach'),
+    ],
+    [
+        InlineKeyboardButton(text='back', callback_data='back_to_start'),
+    ],
+])
